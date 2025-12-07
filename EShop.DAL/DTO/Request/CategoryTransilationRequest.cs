403d5882279a18e442975618797f6dea87e36c87
emanAlawnah <alawanh.eman@gmail.com>
2025-12-07 21:29:26 +0200

    initiliae 3-tire architecture project with loclaization

---
+++ EShop.DAL/DTO/Request/CategoryTransilationRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.DAL.DTO.Request
+{
+    public class CategoryTransilationRequest
+    {
+        public String Name { get; set; }
+
+        public String Language { get; set; }
+
+    }
+}
---
+++ EShop.PL/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using EShop.DAL.Data;
+using EShop.DAL.DTO.Request;
+using EShop.DAL.DTO.Response;
+using EShop.DAL.Models;
+using EShop.PL.Resourses;
+using Mapster;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+
+namespace EShop.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+        private readonly IStringLocalizer<SharedResource> _Localizer;
+
+        public CategoriesController(AplicationDbContext Context,IStringLocalizer<SharedResource> localizer) {
+            _context = Context;
+            _Localizer = localizer;
+        }
+
+        [HttpGet("")]
+        public IActionResult index() {
+            var Categories = _context.Categories.Include(c=>c.Translations).ToList();
+            var Response = Categories.Adapt<List<CategoryResponse>>();
+
+            
+            return Ok(new {massage=_Localizer["Success"].Value, Response });   
+        }
+
+        [HttpPost("")]
+        public IActionResult Create(CategoryRequest request)
+        {
+            var cat = request.Adapt<Category>();
+            _context.Categories.Add(cat);
+            _context.SaveChanges();
+
+            return Ok(new { massage = _Localizer["Success"].Value });
+        }
+
+    }
+}
